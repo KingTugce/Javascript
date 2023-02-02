354a9f1f7a8125322a7123a0a53e387330eb0869
KingTugce <kingstugce@gmail.com>
2023-02-01 18:29:34 -0800

    Javascript day1 && day2

---
+++ day1/main.js
@@ -0,0 +1,301 @@
+// Basics of JavaScript
+
+/*
+Hey Jimbo, Look at my multiline comment!
+Let's put it on the fridge
+Primitive data types in JS: strings, integers, Booleans, floats, arrays, objects (very similar to Python dictionaries)
+Functions in JS
+Loops
+Scripting vs Programming Language
+*/
+
+// String variables
+
+var first_name = 'Tugce'
+
+// Display the value in JavaScript
+console.log(first_name)
+
+// Assign an integer
+var some_number = 33;
+console.log(some_number)
+
+// Show all attributes of a document or id
+console.dir(document)
+
+// Floats
+var some_float = 3.14
+console.log(some_float)
+
+// Array
+var some_array = [1,2,3,4];
+console.log(some_array)
+
+var some_objects = {'Yee' : 'haw'};
+console.log(some_objects)
+
+// javascript hoisting
+
+a_num = 10
+console.log(a_num)
+var a_num
+console.log(a_num)
+
+console.log(some_random_variable) // Still runs but returns Undefined which is basically a 'None Type'; Not a good practice
+var some_random_variable = 'this is a random variable'
+console.log(some_random_variable)
+
+// A better way declaring a variable is by using 'let' or 'const' keywords
+
+let x
+console.log(x)
+x = 3
+console.log(x)
+
+// const y = 5;
+// console.log(y)
+// y = 6;
+// console.log(y)
+
+// Basic math operations
+
+// Addition
+let sum = 5 + 5
+console.log(sum)
+
+// Substraction
+// let diff = 5 - 4
+
+let diff_1 = 5
+let diff_2 = 4
+console.log(diff_1 - diff_2)
+
+// Multiplication and division same as Python, exponents as well
+
+// Some other math operations
+// Finding the Floor of a decimal
+
+let find_floor = Math.floor(5.8)
+console.log(find_floor)
+
+// Alternatively, Math.ceil(n) brings back the ceiling
+
+//Mind Bender
+let crazy_stuff = some_float + '4' // Because this is a string concatenating string to a variable 4 to the end.
+console.log(crazy_stuff)
+
+//Mind Bender 2
+let crazy_stuff_2 = some_float + parseFloat('4')
+console.log(crazy_stuff_2)
+
+//Redeclaration of variables
+var red_ranger = 'Jason'
+var red_ranger = 'tommy'
+console.log(red_ranger)
+
+//Using the let keyword for the same
+let nba_goat = 'Micheal Jordan'
+console.log(nba_goat)
+
+nba_goat = 'Kobe'
+console.log(nba_goat)
+
+//Dont retype let on later variable changes
+
+//Regular named functions
+function addNums(){
+    let num = 4;
+    let num2 = 5;
+
+    return num + num2
+}
+
+console.log(addNums())
+
+let addNum2 = function(num, num2){
+    return num + num2
+}
+
+console.log(addNum2(6,7))
+
+// ES6+ Arrow function in JS
+//Our first example shows how to use a single parameter in an arrow function without parentheses
+// This can only be done with one parameter if you dont want to use parentheses
+
+let cubed = num => {
+    return num**3
+}
+
+// No params in an arrow function
+
+let someFunc = () => {
+    return 5
+}
+
+console.log(cubed(4))
+
+// with parantheses 
+
+let cubed2 = (num) => {
+    return num**3
+}
+
+console.log(cubed2(4))
+
+// If you have multiple parameters you must have parantheses around them
+let toThePower = (num, power) => {
+    return num**power
+}
+console.log(toThePower(4,3))
+
+
+
+console.log(
+    (function(name){
+        let hello = 'Hello ' + name;
+        return hello
+
+
+    })
+('Joel'))
+
+//Control Flow
+// if statements
+
+function determineAge(age){
+    if (age < 18){
+        return 'Minor'
+    } else if (age >= 18 && age < 65){
+        return 'Adult'
+    } else {
+        return 'Senior'
+    }
+}
+
+console.log(determineAge(67))
+
+// Ternary Operators
+
+function determineAge2(age){
+    return (age < 18) ? 'minor' : (age >= 18 && age && age <= 65) ? 'Adult not retired' : 'Elderly person not retired'
+} 
+
+console.log(determineAge2(62))
+
+// LOOOOOOOOPS
+function countByOne(){
+    for(let i = 0 ; i < 20 ; i++){
+        console.log(i)
+    }
+    return 'Counting has stopped'
+}
+console.log(countByOne())
+
+function decreaseByOne(){
+    for(let i = 20; i > 0; i--)
+        console.log(i)
+}
+
+console.log(decreaseByOne())
+
+// WHILE LOOOOOP
+
+function count_with_while(){
+    let i = 0;
+    let text = '';
+    while(i < 10){
+        text += `this number is...${i} \n`
+        i++;
+    };
+    i++
+}
+console.log(count_with_while())
+
+// Do While Loop
+function countWithDoWhile(){
+    let i = 0;
+    let text = '';
+    do {
+        text += `this is a number...${i} \n`;
+        i++
+    }
+    while(i < 10)
+    return text
+}
+
+// Do while will run at least one time!!!
+
+console.log(countWithDoWhile())
+
+//create an array
+let group_of_names = ['jerry', 'ben', 'bash', 'brock', 'misty']
+
+// Index the first position
+console.log(group_of_names[0])
+
+let terry, ben, misty;
+[terry, misty, ben] = group_of_names
+console.log(terry, ben, misty)
+console.log(group_of_names)
+console.log(ben)
+
+// Method 1 for looping through an array
+
+function showAllNames(){
+    for (let i = 0; i < group_of_names.length; i++){
+        console.log(group_of_names[i])
+    }
+    return 'done'
+}
+
+console.log(showAllNames())
+
+//Method 2
+console.log(group_of_names.forEach(element => console.log(element)))
+
+// JS Array method: array.toString()
+console.log(group_of_names.toString())
+console.log(typeof(group_of_names.toString()))
+
+// More methods: map, filter, reduce
+
+// .map()
+let b_names = group_of_names.map( i => {
+    if(i[0] == 'B'){
+        return i
+    } else {
+        return false
+    }
+})
+
+console.log(b_names)
+
+//long form of .map()
+let b_names_test = function (){
+    let test_array = [];
+    for(let i = 0; i < group_of_names.length; i ++){
+        if(group_of_names[i][0] == 'B'){
+            test_array.push(group_of_names[i])
+        }
+    }
+    return test_array
+}
+
+console.log(b_names_test())
+
+// .filter()
+
+let long_names = group_of_names.filter(element => element.length > 4)
+console.log(long_names)
+
+// .reduce()
+
+const nums = [2,4,6,8,10]
+
+let nums_reduced = nums.reduce( (accumulator, current_num) => {
+    return accumulator + current_num
+})
+console.log(nums_reduced)
+
+// You can also use methods like .join, .slice, .search, .splice
+
---
+++ day2/main.js
@@ -0,0 +1,106 @@
+// Continued control flow discussion from previous document
+
+// Switch Case
+
+let day = new Date().getDay();
+
+let literal_day = new Date().toString();
+
+console.log(day)
+console.log(literal_day)
+
+// Switch Case Statement Syntax
+switch(day){
+    case 0:
+        console.log('Go to Church');
+        break;
+    case 1:
+        console.log('Write code... it is Monday!')
+        break;
+    case 2:
+        console.log('Test code... it is Tuesday!')
+        break;
+    case 3:
+        console.log('It is Wednesday my dudes! ')
+        break;
+    case 4:
+        console.log('Write a feature for a project on Thursday')
+        break;
+    case 5:
+        console.log('Its Friday, friday, gotta get down on friday')
+        break;
+    case 6:
+        console.log('Climb a mountain on Saturday')
+        break;
+    default:
+        console.log(' we do not have a case for that')
+}
+
+// Creation of Objects in JS
+// A Simple JS Objects
+
+let person = {
+    name:'John',
+    age: 28,
+    fav_color: 'Red'
+}
+
+// Accessing data in our Object
+console.log(person['age']) // Bracket Notation
+console.log(person.name) // Dot Notation
+
+// Complex JS Object
+let person2 = {
+    name: "Max",
+    age: 31,
+    prog_languages: ['JavaScript', 'Python', 'C++', 'Java'],
+    fav_color: 'Blue,',
+    teams: [{
+        baseball: 'Chicago White Sox',
+        football: 'Chicago Bears',
+        hockey: 'Chicago Blackhawks',
+        basketball:['Chicago Bulls', 'Chicago Sky'],
+        soccer: ['Chicago Fire', 'Naperville Yellowjackets']
+
+    },
+    {
+        baseball: 'Toronto Bluejays',
+        football: 'LA Rams',
+        basketball: 'Milwaukee Bucks',
+        soccer: ['Manchester United', 'Liverpool']
+
+    }
+
+    ]
+}
+
+console.log(person2.prog_languages[2])
+console.log(person2['prog_languages'][2][0])
+console.log(person2.teams[1].soccer[0])
+
+// JS Objects Prototype Methods -- Object Literal
+console.log(Object.keys(person2))
+console.log(Object.values(person2))
+
+// Sad Path of looping through objects in JS
+for (let i = 0; i < person2.length; i ++){
+    console.log(person2[i])
+}
+
+//Happy Path of looping through objects in JS -- LOOking for keys
+for (let i = 0; i < Object.keys(person2).length; i++){
+    console.log(Object.keys(person2)[i])
+}
+
+console.log('aaaaaughghgh')
+// List Values from Person2 Object that are arrays
+for (let i = 0; i < Object.keys(person2).length; i++){
+    if(Array.isArray(Object.values(person2)[i])){
+        console.log(Object.values(person2)[i])
+    }
+}
+
+// Create our own Object prototypes -- using ES6 method syntax
+function Car(make,model,year){
+    
+}
